feae42f5c922ad0d34a9fdef494c62231e808b25
satyaki-02 <84702497+satyaki-02@users.noreply.github.com>
2023-01-10 09:04:29 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "BUILD START"
-python3.10 -m pip install -r requirements.txt
-python3.10 manage.py --noinput --clear
+python3.9 -m pip install -r requirements.txt
+python3.9 manage.py --noinput --clear
 echo "BUILD END"
